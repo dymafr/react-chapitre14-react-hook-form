1a5090b8efc1d9540f7d138d96b8ec0070c69b8f
unknown <quentindevial19@gmail.com>
2022-07-05 13:35:44 +0200

    start c14

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div
+      className="d-flex flex-row justify-content-center align-items-center"
+      style={{ backgroundColor: "#fefefe", height: "100vh", width: "100%" }}
+    >
+      <h1>Hello world !</h1>
+    </div>
+  );
+}
+
+export default App;
