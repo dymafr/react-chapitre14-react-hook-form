97409d7dd302420e1a147f109e9c639a54314db2
unknown <quentindevial19@gmail.com>
2022-07-06 16:01:14 +0200

    lesson 03

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import { useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit, getValues, watch } = useForm();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+    },
+    mode: "onSubmit",
+  });
 
-  watch("name");
+  // watch("name");
 
   function submit(values) {
     console.log(values);
   }
 
-  console.log(getValues());
+  console.log(errors);
 
   return (
     <div
@@ -21,7 +32,47 @@ function App() {
           <label className="mb-5" htmlFor="name">
             Nom
           </label>
-          <input {...register("name")} id="name" type="text" />
+          <input
+            {...register("name", {
+              // disabled: true,
+              minLength: {
+                value: 2,
+                message: "Trop court !",
+              },
+              // required: {
+              //   value: true,
+              //   message: "Le champ est obligatoire",
+              // },
+              // validate(value) {
+              //   if (value === "Jean") {
+              //     return true;
+              //   } else {
+              //     return "Mauvais prénom";
+              //   }
+              // },
+            })}
+            id="name"
+            type="text"
+          />
+          {errors?.name && <p>{errors.name.message}</p>}
+        </div>
+        <div className="d-flex flex-column mb-20">
+          <label className="mb-5" htmlFor="age">
+            Age
+          </label>
+          <input
+            {...register("age", {
+              // onBlur(e) {
+              //   console.log("blur age !");
+              // },
+              // onChange(e) {
+              //   console.log("change age ! ", e);
+              // },
+            })}
+            id="age"
+            type="number"
+          />
+          {errors?.age && <p>{errors.age.message}</p>}
         </div>
         <button className="btn btn-primary">Save</button>
       </form>
