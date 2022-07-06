5884a789d93a728c60a00f3414534db486f6a913
unknown <quentindevial19@gmail.com>
2022-07-06 12:14:49 +0200

    end l2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useForm } from "react-hook-form";
+
 function App() {
+  const { register, handleSubmit, getValues, watch } = useForm();
+
+  watch("name");
+
+  function submit(values) {
+    console.log(values);
+  }
+
+  console.log(getValues());
+
   return (
     <div
       className="d-flex flex-row justify-content-center align-items-center"
       style={{ backgroundColor: "#fefefe", height: "100vh", width: "100%" }}
     >
-      <h1>Hello world !</h1>
+      <form onSubmit={handleSubmit(submit)}>
+        <div className="d-flex flex-column mb-20">
+          <label className="mb-5" htmlFor="name">
+            Nom
+          </label>
+          <input {...register("name")} id="name" type="text" />
+        </div>
+        <button className="btn btn-primary">Save</button>
+      </form>
     </div>
   );
 }
